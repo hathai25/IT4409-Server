c9b9b6e2ce3ea1480ec9b4271a251a5f61b1303d
Bùi Ngọc Thành <buingocthanh2392002@gmail.com>
2023-06-16 13:34:19 +0700

    add code review apis

---
+++ src/modules/reviews/dto/update-review.dto.ts
@@ -0,0 +1,17 @@
+import { IsArray, IsInt, IsNotEmpty, IsOptional, IsString, Max, Min } from "class-validator";
+
+export class UpdateReviewDto {
+    @IsOptional()
+    @IsString()
+    content?: string;
+
+    @IsOptional()
+    @IsInt()
+    @Min(1)
+    @Max(5)
+    rate?: number;
+
+    @IsOptional()
+    @IsArray()
+    medias?: string[];
+}
